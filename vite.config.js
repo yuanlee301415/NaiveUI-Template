899438aa5d12eceaab28ce76576f00317c68a10d
Yuan Lee <yuanlee301415@126.com>
2025-12-16 16:02:14 +0800

    chore: 首页打印版本号&构建时间

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,55 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import pkg from "./package.json";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({mode}) => {
+  const root = process.cwd();
+  const env = loadEnv(mode, root);
+  console.log("ENV:\n", env);
+
+  const {
+    VITE_PORT,
+    VITE_INTERNAL_VERSION,
+    VITE_BASE_API,
+    VITE_PROXY,
+  } = env;
+  const __APP_VERSION__ = [pkg.version, VITE_INTERNAL_VERSION].join(".");
+  const __APP_BUILD_TIME__ = new Date().toISOString();
+
+
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
+
+    server: {
+      port: Number(VITE_PORT),
+      proxy:
+        VITE_PROXY ? {
+            [VITE_BASE_API]: {
+              target: VITE_PROXY,
+              changeOrigin: true,
+              rewrite: (path) => path.replace(VITE_BASE_API, ""),
+            },
+          }
+          : undefined,
+    },
+
+    define: {
+      __APP_VERSION__: JSON.stringify(__APP_VERSION__),
+      __APP_BUILD_TIME__: JSON.stringify(__APP_BUILD_TIME__)
+    }
+  }
 })
+
