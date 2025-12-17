caa98eb5ec062ccc903af70b8d102bc64ab667c1
Yuan Lee <yuanlee301415@126.com>
2025-12-17 10:40:52 +0800

    chore: Add `less`

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {onMounted} from 'vue'
+import { onMounted } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 
@@ -33,40 +33,40 @@ onMounted(() => {
   <RouterView />
 </template>
 
-<style scoped>
+<style scoped lang="less">
 header {
   line-height: 1.5;
   max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+  .logo {
+    display: block;
+    margin: 0 auto 2rem;
+  }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+  nav {
+    width: 100%;
+    font-size: 12px;
+    text-align: center;
+    margin-top: 2rem;
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
+    a {
+      display: inline-block;
+      padding: 0 1rem;
+      border-left: 1px solid var(--color-border);
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
+      &.router-link-exact-active {
+        color: var(--color-text);
+      }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
+      &.router-link-exact-active:hover {
+        background-color: transparent;
+      }
 
-nav a:first-of-type {
-  border: 0;
+      &:first-of-type {
+        border: 0;
+      }
+    }
+  }
 }
 
 @media (min-width: 1024px) {
@@ -74,25 +74,25 @@ nav a:first-of-type {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
-  }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+    .logo {
+      margin: 0 2rem 0 0;
+    }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+    .wrapper {
+      display: flex;
+      place-items: flex-start;
+      flex-wrap: wrap;
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
+      nav {
+        text-align: left;
+        margin-left: -1rem;
+        font-size: 1rem;
 
-    padding: 1rem 0;
-    margin-top: 1rem;
+        padding: 1rem 0;
+        margin-top: 1rem;
+      }
+    }
   }
 }
 </style>
