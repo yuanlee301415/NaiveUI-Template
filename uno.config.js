53f87ff0d331ccbe7892d1b287cc6a4f8b9ec550
Yuan Lee <yuanlee301415@126.com>
2025-12-24 15:24:49 +0800

    chore: 降级 UnoCSS TailwindCSS 预设版本(使用 V3 版本 & 引入 CSS 重置库

--- uno.config.js
+++ uno.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'unocss'
-import presetWind4 from '@unocss/preset-wind4'
+import { defineConfig, presetWind3 } from 'unocss'
 import presetIcons from '@unocss/preset-icons'
 
 export default defineConfig({
   // ...UnoCSS options
   presets: [
-    presetWind4(),
+    presetWind3(),
 
     presetIcons({
       extraProperties: {
