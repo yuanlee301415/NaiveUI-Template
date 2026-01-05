84ae8ff6ee62553f1ed8aaf242b8589227fd3e12
Yuan Lee <yuanlee301415@126.com>
2026-01-05 14:30:04 +0800

    perf: 404 页面使用当前主题

--- src/views/exception/403.vue
+++ src/views/exception/403.vue
@@ -8,17 +8,19 @@ const router = useRouter()
 </script>
 
 <template>
-  <n-result title="403" description="无权限">
-    <template #icon>
-      <SvgIcon local-icon="no-permission" class="text-500px" />
-    </template>
-    <template #footer>
-      <n-flex justify="center" :size="20">
-        <n-button @click="router.replace({ name: HOME_ROUTE_NAME })">返回首页</n-button>
-        <n-button @click="router.go(-1)">返回上一页</n-button>
-      </n-flex>
-    </template>
-  </n-result>
+  <n-layout style="height: 100%">
+    <n-el tag="div" style="color: var(--base-color)" class="size-full flex-center">
+      <n-result title="403" description="无权限">
+        <template #icon>
+          <SvgIcon local-icon="no-permission" class="text-500px" />
+        </template>
+        <template #footer>
+          <n-flex justify="center" :size="20">
+            <n-button @click="router.replace({ name: HOME_ROUTE_NAME })">返回首页</n-button>
+            <n-button @click="router.go(-1)">返回上一页</n-button>
+          </n-flex>
+        </template>
+      </n-result>
+    </n-el>
+  </n-layout>
 </template>
-
-<style scoped lang="less"></style>
